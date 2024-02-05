91b805e80061c2470e863b06b9760ddf131d8511
unknown <birhanuwoeldmariam@gmail.com>
2024-02-05 21:45:10 +0300

    working

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,13 +1,9 @@
 // import React from 'react'
 import axios from 'axios';
 
-// const axios = () => {
-//   return (
-//     <div>axios</div>
-//   )
-// }
+// the orginal one
 const instance = axios.create({
-    baseURL: 'http://api.themoviedb.org/3',
+    baseURL: 'https://api.themoviedb.org/3',
 });
 
 export default instance;
